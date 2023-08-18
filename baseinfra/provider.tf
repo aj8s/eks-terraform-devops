96b361a05a670bbedd5e11fcbac5bbd4928e7f0c
pradipbabar <pradipbaba13@gmail.com>
2023-08-18 13:17:34 +0530

    setup

--- baseinfra/provider.tf
+++ baseinfra/provider.tf
@@ -1,25 +1,28 @@
 terraform {
-    required_version = "~> 1.4.5"
+   
     required_providers{
         aws = {
             source = "hashicorp/aws"
-            version = "~> 4.64.0"
+           
         }
     }
-    backend "s3" {
+    # backend "s3" {
       
-    }
+    # }
+     backend "local" {
+    path = "/home/pradip/infra_ajit/eks-terraform/baseinfra/terraform.tfstate"
+                    }
 }
 
 provider "aws" {
     region = var.aws_region
 }
 
-data "terraform_remote_state" "baseinfra" {
-    backend = "s3"
-    config = {
-        bucket      = "${var.remote_state_bucket}"
-        key         = "${var.remote_state_key}"
-        region      = "${var.aws_region}"
-    }
-}
+# data "terraform_remote_state" "baseinfra" {
+#     backend = "s3"
+#     config = {
+#         bucket      = "${var.remote_state_bucket}"
+#         key         = "${var.remote_state_key}"
+#         region      = "${var.aws_region}"
+#     }
+# }
